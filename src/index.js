545ad28efe188fe0320e10c38294293dac523f27
Ayush Singh <chiragsingh9389755@gmail.com>
2026-04-02 23:31:53 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -17,20 +17,7 @@ const allowedOrigins = [
   process.env.FRONTEND_URL,
 ].filter(Boolean);
 
-// app.use(
-//   cors({
-//     origin: function (origin, callback) {
-//       // ✅ allow postman / server-to-server calls (no origin)
-//       if (!origin) return callback(null, true);
 
-//       if (allowedOrigins.includes(origin)) return callback(null, true);
-
-//       return callback(new Error("Not allowed by CORS: " + origin));
-//     },
-//     credentials: true,
-//     methods: ["GET", "POST", "PUT", "DELETE"],
-//   })
-// );
 
 app.use(
   cors({
